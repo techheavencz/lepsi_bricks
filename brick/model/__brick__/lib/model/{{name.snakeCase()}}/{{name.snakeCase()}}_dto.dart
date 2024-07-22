a3166c1ee81b565d2b7bde7f86b7549c759d5bd0
hory <horejsja@gmail.com>
2024-07-22 15:45:58 +0200

    updated dto

--- brick/model/__brick__/lib/model/{{name.snakeCase()}}/{{name.snakeCase()}}_dto.dart
+++ brick/model/__brick__/lib/model/{{name.snakeCase()}}/{{name.snakeCase()}}_dto.dart
@@ -8,9 +8,7 @@ part '{{#snakeCase}}{{name}}{{/snakeCase}}_dto.g.dart';
 @JsonSerializable(explicitToJson: true)
 class {{#pascalCase}}{{name}}{{/pascalCase}}Dto extends TransportEntity {
 
-  {{#pascalCase}}{{name}}{{/pascalCase}}Dto({
-
-  });
+  {{#pascalCase}}{{name}}{{/pascalCase}}Dto();
 
   factory {{#pascalCase}}{{name}}{{/pascalCase}}Dto.fromJson(Map<String, dynamic> json) => _${{#pascalCase}}{{name}}{{/pascalCase}}DtoFromJson(json);
   Map<String, dynamic> toJson() => _${{#pascalCase}}{{name}}{{/pascalCase}}DtoToJson(this);
