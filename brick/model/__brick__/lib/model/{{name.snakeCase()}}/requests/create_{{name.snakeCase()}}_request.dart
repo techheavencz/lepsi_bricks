d68c82f84a7a5f41efde073e8386fb397ad0c02e
hory <horejsja@gmail.com>
2024-07-22 15:54:40 +0200

    updated create request

--- brick/model/__brick__/lib/model/{{name.snakeCase()}}/requests/create_{{name.snakeCase()}}_request.dart
+++ brick/model/__brick__/lib/model/{{name.snakeCase()}}/requests/create_{{name.snakeCase()}}_request.dart
@@ -1,13 +1,13 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:lepsi_restaurace_models/http/request/api_request_create.dart';
-import 'package:lepsi_restaurace_models/model/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}.dart';
+import 'package:lepsi_restaurace_models/model/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_dto.dart';
 
 part 'create_{{#snakeCase}}{{name}}{{/snakeCase}}_request.g.dart';
 
 @JsonSerializable(explicitToJson: true)
-class Create{{#pascalCase}}{{name}}{{/pascalCase}}Request extends ApiRequestCreate<{{#pascalCase}}{{name}}{{/pascalCase}}> {
+class Create{{#pascalCase}}{{name}}{{/pascalCase}}Request extends ApiRequestCreate<{{#pascalCase}}{{name}}{{/pascalCase}}Dto> {
 
-  Create{{#pascalCase}}{{name}}{{/pascalCase}}Request(List<{{#pascalCase}}{{name}}{{/pascalCase}}> entities):
+  Create{{#pascalCase}}{{name}}{{/pascalCase}}Request(List<{{#pascalCase}}{{name}}{{/pascalCase}}Dto> entities):
         super(entities: entities);
 
   factory Create{{#pascalCase}}{{name}}{{/pascalCase}}Request.fromJson(Map<String, dynamic> json) => _$Create{{#pascalCase}}{{name}}{{/pascalCase}}RequestFromJson(json);
